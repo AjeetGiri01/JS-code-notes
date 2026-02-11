51cecd7222327393159381686f13ea1c4ff4b9ef
AjeetGiri01 <giriajeet543@gmail.com>
2026-02-11 10:37:19 +0530

    did basics of DOM

---
+++ 06_dom/script.js
@@ -0,0 +1,11 @@
+let heading = document.getElementById("heading"); //returns the value of h1
+console.dir(heading);
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
+// document.getElementsByTagName
+let firstEl = document.querySelector("p"); //1st select
+console.dir(firstEl);
+
+let elements = document.querySelectorAll("p"); // select all .myclass/#myid/p
+console.dir(elements);
