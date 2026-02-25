4e7470695a2a8cfe8d750e1d7f6a84591b982f18
AjeetGiri01 <giriajeet543@gmail.com>
2026-02-25 22:13:44 +0530

    learn some style and other dom manipulation

--- 07_dom2/script.js
+++ 07_dom2/script.js
@@ -15,6 +15,16 @@ console.log(para.getAttribute("class"));
 console.log(para.setAttribute("class", "newClass"));
 
 
-//
+//style..
 div.style.backgroundColor = "green";
 div.style.fontSize = "26px";
+div.innerText = "Hello";
+// div.style.visibility = "hidden";
+
+
+//insert elements.. 1.->let el=document.createElement("div")  2.--> add
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+console.log(newBtn);
+
+//node.append(el)-->adds at the end of node(inside)
