6822f8c63bf07719a457da89b0e640d010d5a55a
AjeetGiri01 <giriajeet543@gmail.com>
2026-07-21 18:26:16 +0530

    learing about the ajax, https verbs, headers and the api requests

--- 12_async_fun/app.js
+++ 12_async_fun/app.js
@@ -89,3 +89,49 @@ let student = {
 console.log(JSON.stringify(student));
 
 //testing API requests --> hoppscotch and postman
+
+
+//-------------------------------------------
+//api request
+// let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log("data1 = ", data.fact);
+//     return fetch(url);
+// })
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data2) => {
+//     console.log("data2 = ", data2.fact);
+// })
+// .catch((err) => {
+//     console.log("ERROR - ", err);
+// });
+
+// console.log("Am I happy?");
+
+
+//-----------------------------------------------
+//fetch with async-await
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    } catch (e) {
+        console.log("error - ", e);
+    }
+
+    console.log("bye");
+}
