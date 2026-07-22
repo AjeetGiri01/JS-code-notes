ee1103bb41c8d2fa6fab7d5c0c7c9bf9af1b74a7
AjeetGiri01 <giriajeet543@gmail.com>
2026-07-22 17:58:18 +0530

    explored some more concept of the api and sending headers with api request

--- 13_axios_lib/app.js
+++ 13_axios_lib/app.js
@@ -1,21 +1,37 @@
-//axios -> library to make http request
+// //axios -> library to make http request
 
-let btn = document.querySelector("button");
-btn.addEventListener("click", async () => {
-    let fact = await getFacts();
-    // console.log(fact);
-    let p = document.querySelector("#result");
-    p.innerText = fact;
-});
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async () => {
+//     let fact = await getFacts();
+//     // console.log(fact);
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+// });
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
-async function getFacts() {
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//         return res.data.fact;
+//     } catch (e) {
+//         console.log("error - ", e);
+//         return "No fact found";
+//     }
+// }
+
+
+
+//-----------------------------------------------
+//sending headers with api request
+const url = "https://icanhazdadjoke.com/";
+
+async function getJokes() {
     try {
-        let res = await axios.get(url);
-        return res.data.fact;
-    } catch (e) {
-        console.log("error - ", e);
-        return "No fact found";
+        const config = { headers: {Accept : "Application/json"} };
+        let res = await axios.get(url, config);
+        console.log(res.data);
+    } catch(err) {
+        console.log(err);
     }
 }
